5fd4307e6dc1a2343e5602418e95f93d14fffd70
sravya12395 <bubbly.sravya23@gmail.com>
2026-08-14 13:28:54 +0530

    variables commit

--- src/basics/Variables.java
+++ src/basics/Variables.java
@@ -4,6 +4,11 @@ public class Variables {
 
     public static void main (String[] args) {
         int age = 31;
+        String name = "Sravya";
+        float weight = 56.8f;
+
+        System.out.println("My name is: " + name);
         System.out.println("My age is: " + age);
+        System.out.println("My weight is: " + weight);
     }
 }
